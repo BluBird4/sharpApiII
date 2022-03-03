95402b173aaa27ca7be2c0adb4347e362b2a17dd
BluBird4 <alhussienrehan@hotmail.com>
2022-03-03 23:18:08 +0200

    Final

--- dist/resize.js
+++ dist/resize.js
@@ -16,56 +16,114 @@ const express_1 = __importDefault(require("express"));
 const fs_1 = __importDefault(require("fs"));
 const sharp_1 = __importDefault(require("sharp"));
 const route = express_1.default.Router();
-const middleware = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    const heigh = +req.query.height;
-    const widt = +req.query.width;
-    const filename = req.query.filename;
-    if (!filename) {
-        res.send("Image name not provided");
-    }
-    else if (filename.split(".")[1] !== "jpg") {
-        res.send("We only support .jpg images at the moment");
-    }
-    else {
-        const filepath = filename;
-        const exists = fs_1.default.existsSync(filepath);
-        const jpname = filename.split(".")[0];
-        const name = jpname + "x" + widt + "x" + heigh + ".jpg";
-        if (exists) {
-            const searchname = jpname + "x" + widt + "x" + heigh + ".jpg";
-            const exist = fs_1.default.existsSync(searchname);
-            if (exist) {
-                const rsss = __dirname + "/" + searchname;
-                res.sendFile(rsss);
-            }
-            else {
-                // working on the async and await functions in the code : Filepath name
-                function resizeImage() {
-                    return __awaiter(this, void 0, void 0, function* () {
-                        try {
-                            yield (0, sharp_1.default)(filepath)
-                                .resize({
-                                width: widt,
-                                height: heigh,
-                            })
-                                .toFile(name);
-                        }
-                        catch (error) {
-                            console.log(error);
-                        }
-                    });
+function middleware(req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const heigh = +req.query.height;
+        const widt = +req.query.width;
+        const filename = req.query.filename;
+        if (!filename) {
+            res.send("Image name not provided");
+        }
+        else if (filename.split(".")[1] !== "jpg") {
+            res.send("We only support .jpg images at the moment");
+        }
+        else {
+            const filepath = filename;
+            const exists = fs_1.default.existsSync(filepath);
+            const jpname = filename.split(".")[0];
+            const name = jpname + "x" + widt + "x" + heigh + ".jpg";
+            if (exists) {
+                const searchname = jpname + "x" + widt + "x" + heigh + ".jpg";
+                const exist = fs_1.default.existsSync(searchname);
+                if (exist) {
+                    const rsss = __dirname + "/" + searchname;
+                    res.sendFile(rsss);
                 }
-                yield resizeImage();
-                const rss = __dirname + "/" + name;
-                res.sendFile(rss);
+                else {
+                    // working on the async and await functions in the code : Filepath name
+                    function resizeImage() {
+                        return __awaiter(this, void 0, void 0, function* () {
+                            try {
+                                yield (0, sharp_1.default)(filepath)
+                                    .resize({
+                                    width: widt,
+                                    height: heigh,
+                                })
+                                    .toFile(name);
+                            }
+                            catch (error) {
+                                console.log(error);
+                            }
+                        });
+                    }
+                    yield resizeImage();
+                    const rss = __dirname + "/" + name;
+                    res.sendFile(rss);
+                }
+            }
+            if (!exists) {
+                res.send("File Doesn't exist");
             }
         }
-        if (!exists) {
-            res.send("File Doesn't exist");
+        next();
+    });
+}
+/*
+const middleware = async (
+    req: express.Request,
+    res: express.Response,
+    next: Function
+): Promise<void> => {
+
+  const heigh: number = +req.query.height;
+  const widt: number = +req.query.width;
+  const filename = req.query.filename as string;
+
+    if (!filename) {
+    res.send("Image name not provided");
+  } else if (filename.split(".")[1] !== "jpg") {
+    res.send("We only support .jpg images at the moment");
+  } else {
+    const filepath = filename;
+    const exists = fs.existsSync(filepath);
+    const jpname = filename.split(".")[0];
+    const name = jpname + "x" + widt + "x" + heigh + ".jpg";
+    if (exists) {
+      const searchname = jpname + "x" + widt + "x" + heigh + ".jpg";
+      const exist = fs.existsSync(searchname);
+      if (exist) {
+        const rsss = __dirname + "/" + searchname;
+        res.sendFile(rsss);
+      } else {
+         // working on the async and await functions in the code : Filepath name
+        async function resizeImage() {
+          try {
+            await sharp(filepath)
+              .resize({
+                width: widt,
+                height: heigh,
+              })
+              .toFile(name);
+          } catch (error) {
+            console.log(error);
+          }
         }
+
+        await resizeImage();
+
+        const rss = __dirname + "/" + name;
+        res.sendFile(rss);
+      }
     }
-    next();
-});
+    if (!exists) {
+      res.send("File Doesn't exist");
+    }
+  }
+
+  next();
+
+};
+*/
 route.get("/images", middleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     console.log("Image resizing api !");
 }));
--- src/resize.ts
+++ src/resize.ts
@@ -1,9 +1,59 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import fs from "fs";
 import path from "path";
 import sharp from "sharp";
 const route = express.Router();
 
+async function middleware(req: Request, res: Response, next: NextFunction){
+  const heigh: number = +req.query.height;
+  const widt: number = +req.query.width;
+  const filename = req.query.filename as string;
+
+    if (!filename) {
+    res.send("Image name not provided");
+  } else if (filename.split(".")[1] !== "jpg") {
+    res.send("We only support .jpg images at the moment");
+  } else {
+    const filepath = filename;
+    const exists = fs.existsSync(filepath);
+    const jpname = filename.split(".")[0];
+    const name = jpname + "x" + widt + "x" + heigh + ".jpg";
+    if (exists) {
+      const searchname = jpname + "x" + widt + "x" + heigh + ".jpg";
+      const exist = fs.existsSync(searchname);
+      if (exist) {
+        const rsss = __dirname + "/" + searchname;
+        res.sendFile(rsss);
+      } else {
+         // working on the async and await functions in the code : Filepath name
+        async function resizeImage() {
+          try {
+            await sharp(filepath)
+              .resize({
+                width: widt,
+                height: heigh,
+              })
+              .toFile(name);
+          } catch (error) {
+            console.log(error);
+          }
+        }
+
+        await resizeImage();
+
+        const rss = __dirname + "/" + name;
+        res.sendFile(rss);
+      }
+    }
+    if (!exists) {
+      res.send("File Doesn't exist");
+    }
+  }
+
+  next();
+}
+
+/*
 const middleware = async (
     req: express.Request,
     res: express.Response,
@@ -58,6 +108,7 @@ const middleware = async (
   next();
 
 };
+*/
 
 route.get("/images", middleware , async (req: Request, res: Response) => {
   console.log("Image resizing api !");
